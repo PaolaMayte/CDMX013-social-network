364c9081a8743fd223040f3d1e08115ea5c33057
PaolaMayte <109124235+PaolaMayte@users.noreply.github.com>
2022-09-30 10:27:55 -0500

    SPA completa y corregida

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,25 @@
-import { onNavigate } from './main.js';
+import { onNavigate } from '../main.js';
 
-export const Welcome = () => {
+export const welcome = () => {
     const div = document.createElement('div');
-    const title = document.createElement ('h2');
+    const title = document.createElement('h2');
     const buttonLogin = document.createElement('button');
-    const buttonSingup = document.createElement('button');
+    const buttonSignup = document.createElement('button');
 
     buttonLogin.textContent = 'LOG IN';
-    buttonSingup.textContent = 'SING UP'
+    buttonSignup.textContent = 'SIGN UP'
     title.textContent = 'ENIGMA';
 
-    div.append (title, buttonLogin, buttonSingup);
+    buttonLogin.addEventListener('click', () => {
+        onNavigate('/login');
+    })
+
+    buttonSignup.addEventListener('click', () => {
+        onNavigate('/signup');
+    })
+
+
+    div.append(title, buttonLogin, buttonSignup);
 
     return div;
 };
